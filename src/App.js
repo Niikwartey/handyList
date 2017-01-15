247db517f34fb52841e7d592460e5dd12347a4ab
Derick Quartey <niikwartey1@gmail.com>
2017-01-15 16:12:38 -0500

    fully converted to react

--- src/App.js
+++ src/App.js
@@ -4,17 +4,57 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    return(
+      <div>
+        <div id="jumbo-search" className="ui center aligned padded segment">
+          <div className="jumbotron">
+            <h2>What are you looking for?</h2>
+            <div className="ui icon huge input">
+              <input type="text" placeholder="eg. TV Repair, Plumbing etc."/>
+              <i className="inverted green circular search link icon"></i>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="ui center aligned segment">
+          <div className="ui container">
+            <div className="ui grid">
+              <div id="listings" className="four column doubling row">
+                 {/* each item in listing */}
+                <div className="column">
+                  <div className="ui centered card">
+                    <div className="image">
+                      <img src="http://www.coyotehandyman.com/images/coyote%20handyman%20header.jpg"/>
+                    </div>
+                    <div className="content">
+                      <div className="header">
+                        $149 for a Home Interior Mold and Water Damage Inspection and $149 Credit
+                      </div>
+                      <div className="meta">
+                        Offered by: <a href="#">24/7 Property Cleaning & Restoration</a>
+                      </div>
+
+                    </div>
+                    <div className="extra content price">
+                      <span className="original-price">
+                        $1000
+                      </span>
+                      <span className="member-price">
+                        $799
+                      </span>
+                    </div>
+
+                    <a className="ui inverted orange bottom attached button" href="#">
+                      View Offer
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
